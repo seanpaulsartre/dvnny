b8a308fc158f12596ae6aebb4194a8be730dca67
Emma <doodybrainz@gmail.com>
2016-03-13 13:27:07 -0400

    love-time to spec

--- source/javascripts/scroll.js
+++ source/javascripts/scroll.js
@@ -29,6 +29,32 @@ var Site = {
          $scroll.css({ top: offsetTop });
        });
     });
+
+    var expires = new Date();
+    expires.setSeconds(expires.getSeconds() + 193212890);
+    var counter = setInterval(timer, 1);
+
+    function timer() {
+      var timeDiff = expires - new Date();
+      if (timeDiff <= 0) {
+        clearInterval(counter);
+        document.getElementById("timer").innerHTML = "00:00";
+        return;
+      }
+
+      var minutes = new Date(timeDiff).getMinutes();
+      var seconds = new Date(timeDiff).getSeconds();
+      var milliSeconds = (new Date(timeDiff).getMilliseconds()/10).toFixed(0);
+      var minutes = minutes < 10 ? "0" + minutes: minutes;
+      var seconds = seconds < 10 ? "0" + seconds: seconds;
+      var milliSeconds = milliSeconds < 10 ? "0" + milliSeconds: milliSeconds;
+      var distance = seconds * 100000;
+      var hoursLeft = minutes * 49040;
+
+      document.getElementById("timer").innerHTML = minutes + ":" + seconds + ":" + milliSeconds;
+      document.getElementById("distance").innerHTML = distance;
+      document.getElementById("hours").innerHTML = "000000" + hoursLeft;
+    }
   },
 
   fillBrowser: function() {
